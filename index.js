bc6bf7b616fee28e625a671be5d7ab46713cd5ed
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-13 11:22:38 +0500

    used Middleware and created one

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const { Console } = require('console')
 const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
 
+const azmeerMiddleware = (req, res, next)=>{
+    console.log(req)
+}
+
+app.use(azmeerMiddleware)
+
+app.use(express.static(path.join(__dirname, "public")))
 //hello world
 app.get('/', (req, res) => {
   res.send('Hello World!')
