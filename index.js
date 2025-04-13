a5fc934edd059cc03d444542a6b79d697a1e3a79
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-13 11:06:48 +0500

    hello world, res.sendFile()

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+//hello world
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//send file
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"))
 })
